832639b648b260a063867deae3b153975bdc2cb8
Stewart Lynch <slynch@createchsol.com>
2021-05-18 08:49:01 -0700

    Initial Commit

---
+++ Happy Facts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Happy Facts
+//
+//  Created by Stewart Lynch on 2021-05-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Happy Facts/Happy_FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Happy_FactsApp.swift
+//  Happy Facts
+//
+//  Created by Stewart Lynch on 2021-05-18.
+//
+
+import SwiftUI
+
+@main
+struct Happy_FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
